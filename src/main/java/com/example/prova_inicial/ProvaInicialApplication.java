1344aef4632a84fb8e2433c9e62ac8c383c6755c
matheusvmd <matheus.vicco173016@gmail.com>
2025-09-17 10:44:38 -0300

    commit-inicial

---
+++ src/main/java/com/example/prova_inicial/ProvaInicialApplication.java
@@ -0,0 +1,13 @@
+package com.example.prova_inicial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaInicialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvaInicialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/prova_inicial/ProvaInicialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prova_inicial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaInicialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
